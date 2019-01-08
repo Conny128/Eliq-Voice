305deb6b1fda2bdc61306655dd384e491c918344
Conny128 <conny.berg@live.com>
2019-01-08 10:03:02 +0100

    Version 1.0

--- Eliq_NOW.py
+++ Eliq_NOW.py
@@ -3,61 +3,88 @@ import json
 import time
 
 
+import win32com.client as wincl
+speak = wincl.Dispatch("SAPI.SpVoice")
+speak.Speak("Hello World")
+
+#from gtts import gTTS
+#import os
+#gTTS(text='Good morning', lang='en')
+
+
+
 Eliq_list = []
-b=0
-a=0
-length = 0
-power_index=0
+power_value = 0
+power_value_int = 0
+prompt=0
 
-response = requests.get ("https://my.eliq.io/api/datanow?accesstoken=745109ac4bed4c318ff47b06e5285f95&channelid=32217")
+present_time = time.localtime()
+print (present_time)
+print ("How many minutes interval (recomended 10 minutes)?")
+prompt=input()
+offset_tid=int(prompt)
+min_tid = present_time[4]
+print (min_tid)
+max_tid = min_tid + offset_tid
+print (max_tid)
 
-Eliq_just_NOW_string = str (response.json())
-#print (Eliq_just_NOW_string)
-Eliq_list.insert(0,Eliq_just_NOW_string.split(",")[0])
-Eliq_list.insert(1,Eliq_just_NOW_string.split(",")[1])
-Eliq_list.insert(2,Eliq_just_NOW_string.split(",")[2])
+for i in range (offset_tid):
+    print(i)
 
-Eliq_list.sort()
-
-Eliq_array_0 = str (Eliq_list[0])
-if Eliq_array_0[2:11] == 'channelid':
-    pass
-    #print ('CHANNELID')
-    #print ( Eliq_array_0[2:11] )
-if Eliq_array_0[2:7] == 'power':
-    print ( Eliq_array_0[2:7] )
-    print ( Eliq_array_0[9:14] )
-if Eliq_array_0[2:13] == 'createddate':
-    pass
-    #print ('CREATEDATE')
-    #print ( Eliq_array_0[2:13] )
+    response = requests.get ("https://my.eliq.io/api/datanow?accesstoken=745109ac4bed4c318ff47b06e5285f95&channelid=32217")
     
-Eliq_array_1 = str (Eliq_list[1])
-if Eliq_array_1[2:11] == 'channelid':
-    pass
-    #print ('CHANNELID')
-    #print ( Eliq_array_1[2:11] )
-if Eliq_array_1[2:7] == 'power':
-    print ( Eliq_array_1[2:7] )
-    print ( Eliq_array_1[9:14] )
-if Eliq_array_1[2:13] == 'createddate':
-    pass
-    #print ('CREATEDATE')
-
-
-    #print ( Eliq_array_1[2:13] )
+    Eliq_just_NOW_string = str (response.json())
+    #print (Eliq_just_NOW_string)
+    Eliq_list.insert(0,Eliq_just_NOW_string.split(",")[0])
+    Eliq_list.insert(1,Eliq_just_NOW_string.split(",")[1])
+    Eliq_list.insert(2,Eliq_just_NOW_string.split(",")[2])
     
-Eliq_array_2 = str (Eliq_list[2])
-if Eliq_array_2[2:11] == 'channelid':
-    pass
-    #print ('CHANNELID')
-    #print ( Eliq_array_2[2:11] )
-if Eliq_array_2[2:7] == 'power':
-    print ( Eliq_array_2[2:7] )
-    print ( Eliq_array_2[9:14] )
-if Eliq_array_2[2:13] == 'createddate':
-    pass
-    #print ('CREATEDATE')
-    #print ( Eliq_array_2[2:13] )
-
-
+    Eliq_list.sort()
+    
+    Eliq_array_0 = str (Eliq_list[0])
+    if Eliq_array_0[2:11] == 'channelid':
+        pass
+    
+    if Eliq_array_0[2:7] == 'power':
+        print ( Eliq_array_0[2:7] )
+        print ( Eliq_array_0[9:14] )
+        power_value = Eliq_array_0[9:14]
+        power_value_int = int (float (power_value))
+    if Eliq_array_0[2:13] == 'createddate':
+        pass
+        
+    Eliq_array_1 = str (Eliq_list[1])
+    if Eliq_array_1[2:11] == 'channelid':
+        pass
+    
+    if Eliq_array_1[2:7] == 'power':
+        print ( Eliq_array_1[2:7] )
+        print ( Eliq_array_1[9:14] )
+        power_value = Eliq_array_1[9:14]
+        power_value_int = int (float (power_value))
+    
+    if Eliq_array_1[2:13] == 'createddate':
+        pass
+        
+    Eliq_array_2 = str (Eliq_list[2])
+    if Eliq_array_2[2:11] == 'channelid':
+        pass
+    
+    if Eliq_array_2[2:7] == 'power':
+        print ( Eliq_array_2[2:7] )
+        print ( Eliq_array_2[9:14] )
+        power_value = Eliq_array_2[9:14]
+        power_value_int = int (float (power_value))
+    
+    if Eliq_array_2[2:13] == 'createddate':
+        pass
+    
+    if power_value_int >= 2000:
+        speak.Speak(power_value)
+        speak.Speak ("Warning!")
+    else:
+        speak.Speak(power_value)
+        speak.Speak ("Good!")
+        
+    time.sleep(10)
+    
