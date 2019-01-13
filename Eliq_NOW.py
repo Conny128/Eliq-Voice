61c117f7d890e18be4005144cb0d6a3ba1f54ced
Conny128 <conny.berg@live.com>
2019-01-13 15:47:21 +0100

    Third commit

--- Eliq_NOW.py
+++ Eliq_NOW.py
@@ -8,33 +8,38 @@ import time
 
 import win32com.client as wincl
 speak = wincl.Dispatch("SAPI.SpVoice")
-speak.Speak("Hello World")
+speak.Speak("Hello from Eliq")
 
+Eliq_list = []
 power_value = 0
 power_value_int = 0
 prompt=0
 Eliq_just_NOW ={}
+accesstoken = "xxxxxxxxxxxxxxxxxxxxxx"
 
-print ("How many minutes interval (recomended 10 minutes)? : ")
-prompt=input()
+prompt = input ("What is the warning limit in Watts: ") #Say warning for power use over this limmit in Watts
+level_warning = int(prompt)
+
+prompt = input ("How many minutes between each? : ")
 offset_tid=int(prompt)
 
-for i in range (offset_tid):
-    response = requests.get ("https://my.eliq.io/api/datanow?accesstoken=aaaaaaaaaaaaaaaaaaaa&channelid=ccccc")
+Eliq_request_string = ('https://my.eliq.io/api/datanow?accesstoken={}&channelid=32217'.format(accesstoken))
+
+while True:
+    response = requests.get (Eliq_request_string)
     Eliq_just_NOW = (response.json())
     power_value = Eliq_just_NOW['power']
     power_value_int = int (float (power_value))
     power_str = (f'Power is {power_value_int} Watts') 
     
-    
-    if power_value_int >= 2000:
-        print (power_str)
+    print (power_str)
+   
+    if power_value_int > level_warning:
         speak.Speak(power_str)
         speak.Speak ("Warning!")
     else:
-        print (power_str)
         speak.Speak(power_str)
         speak.Speak ("Good!")
         
-    time.sleep(60)
+    time.sleep(offset_tid*60)
     
